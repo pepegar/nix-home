9e0fd42cd7a45b7c344b905e130dbd5ab3cc7650
Pepe García <pepe@pepegar.com>
2019-07-24 12:40:32 +0200

    create macbook config

---
+++ machines/macbook.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    cfg/emacs
+    cfg/email.nix
+    cfg/fzf.nix    
+    cfg/git.nix
+    cfg/ssh.nix
+    cfg/xresources.nix
+    cfg/zsh.nix
+  ];
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.gitAndTools.gitFull
+    pkgs.gnupg
+    pkgs.pass
+    pkgs.htop
+    pkgs.jdk8
+    pkgs.nix-prefetch-scripts
+    pkgs.openvpn
+  ];
+}
