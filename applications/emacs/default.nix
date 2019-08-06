12ba2e11faaf5dcb2a8fdabc81abe0eb0e1fa1a4
Pepe García <pepe@pepegar.com>
2019-08-06 12:49:42 +0200

    delete emacs stuff

--- applications/emacs/default.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}:
-
-{
-  home.file.".emacs.d/init.el".source = ./init.el;
-  home.file.".emacs.d/lisp/pragmatapro.el".source = ./lisp/pragmatapro.el;
-  home.file.".emacs.d/lisp/xresources-theme.el".source = ./lisp/xresources-theme.el;
-  home.file.".emacs.d/lisp/hydra-posframe.el".source = ./lisp/hydra-posframe.el;  
-}
