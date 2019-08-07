eb88e0b2d1cecd2589c68c1f4962724b76bf92c4
Pepe García <pepe@pepegar.com>
2019-08-07 14:21:48 +0200

    add key to gpg agent

--- services/gpg-agent.nix
+++ services/gpg-agent.nix
@@ -4,6 +4,13 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    defaultCacheTtl = 1800;
+    verbose = true;
+    sshKeys = ["80E02EE85A7D07C506F2D5AE512234A2490E6842"];
+
+    extraConfig = ''
+      allow-emacs-pinentry
+      allow-loopback-pinentry
+    '';
+
   };
 }
