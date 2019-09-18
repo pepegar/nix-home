c67750052d1f8a21d81dbafcf4f521d909780b43
Pepe García <pepe@pepegar.com>
2019-09-18 21:45:18 +0200

    removed ssh stuff from ggp agent

--- services/gpg-agent.nix
+++ services/gpg-agent.nix
@@ -3,9 +3,7 @@
 {
   services.gpg-agent = {
     enable = true;
-    enableSshSupport = true;
     verbose = true;
-    sshKeys = ["80E02EE85A7D07C506F2D5AE512234A2490E6842"];
 
     extraConfig = ''
       allow-emacs-pinentry
