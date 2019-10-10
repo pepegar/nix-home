6922ff169c126f9c6a5ac18c1aebb669d841096c
Pepe García <pepe@pepegar.com>
2019-10-10 10:35:21 +0200

    update metals-emacs

--- pkgs/metals-emacs/default.nix
+++ pkgs/metals-emacs/default.nix
@@ -1,24 +1,36 @@
-{ stdenv, pkgs, ... }:
+{ stdenv, makeWrapper, coursier, jdk, jre, ... }:
 
 let
   baseName = "metals-emacs";
   version = "0.7.6";
+  deps = stdenv.mkDerivation {
+    name = "${baseName}-deps-${version}";
+    buildCommand = ''
+    export COURSIER_CACHE=$(pwd)
+    ${coursier}/bin/coursier fetch org.scalameta:metals_2.12:${version} > deps
+    mkdir -p $out/share/java
+    cp $(< deps) $out/share/java/
+    '';
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash     = "02ra7lvwh4b3d3d56cypi066kgibz5m0pvv3lqa0rffah8h7pyqi";
+  };
 in
 stdenv.mkDerivation {
-    name = "${baseName}-${version}";
-    buildCommand = ''
-      export COURSIER_CACHE=$(pwd)
+  name = "${baseName}-${version}";
 
-      ${pkgs.coursier}/bin/coursier bootstrap \
-        --java-opt -Xss4m \
-        --java-opt -Xms100m \
-        --java-opt -Dmetals.client=emacs \
-        org.scalameta:metals_2.12:${version} \
-        -r bintray:scalacenter/releases \
-        -r sonatype:snapshots \
-        -o ${baseName} -f
+  buildInputs = [jdk makeWrapper deps];
 
-      mkdir -p $out/bin
-      mv ${baseName} $out/bin
-    '';
+  doCheck = true;
+
+  phases = ["installPhase"];
+
+  installPhase = ''
+  makeWrapper ${jre}/bin/java $out/bin/${baseName} \
+    --add-flags "-Xss4m -Xms100m -Dmetals.client=emacs -cp $CLASSPATH scala.meta.metals.Main"
+  '';
+
+  # checkPhase = ''
+  # $out/bin/${baseName} --version | grep -q "${version}""
+  # '';
 }
