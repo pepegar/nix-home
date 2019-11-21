d7a6073517a7ccc97199e07cd45d6de519f3ee80
Pepe García <pepe@pepegar.com>
2019-11-21 23:43:03 +0100

    somehow signing with git broke... :shrug:

--- cfg/git.nix
+++ cfg/git.nix
@@ -7,11 +7,6 @@
     userName = "Pepe García";
     userEmail = "pepe@pepegar.com";
 
-    signing = {
-      key = "6230C44BA0B0FF6608D87F2CC4165E53FFD5B1C6";
-      signByDefault = true;
-    };
-
     extraConfig = {
       github = {
         user = "pepegar";
