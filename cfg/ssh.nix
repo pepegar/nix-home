a829b3a8a8a7da73bfd863c676d1bc3038f6e670
Pepe García <pepe@pepegar.com>
2019-12-27 22:46:04 +0100

    add mongo tunnels to ssh

--- cfg/ssh.nix
+++ cfg/ssh.nix
@@ -9,13 +9,22 @@
         hostname = "${name}-bastion01.packitos.com";
         port = 22822;
         user = "packdev";
-      }; in {
-
+      };
+      packHostTunnelMongo = name : packHost name // {
+        extraOptions = {
+          "LocalForward" = "27017 mongo.service.consul:27017";
+        };
+      };
+    in {
         # all these require VPN
         xteam = packHost "xteamqa";
         fraggle = packHost "fraggleteamqa";
         lemmings = packHost "lemmingsteamqa";
         gremlin = packHost "gremlin";
+        xteam-mongo = packHostTunnelMongo "xteamqa";
+        fraggle-mongo = packHostTunnelMongo "fraggleteamqa";
+        lemmings-mongo = packHostTunnelMongo "lemmingsteamqa";
+        gremlin-mongo = packHostTunnelMongo "gremlin";
 
         keychain = {
           host = "*";
