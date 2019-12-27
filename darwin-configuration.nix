b5c6c22c2851518a74550f6f60739ae4e0116860
Pepe García <pepe@pepegar.com>
2019-12-27 17:34:32 +0100

    add darwin config

---
+++ darwin-configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs;
+    [];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin-configuration.nix";
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.bash.enable = true;
+  programs.zsh.enable = true;
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 4;
+  nix.buildCores = 4;
+  nix.useDaemon = true;
+  services.activate-system.enable = true;
+
+  launchd.user.agents = {
+    mbsync = {
+      command = "${pkgs.isync}/bin/mbsync -a";
+      environment.HOME = "";
+      environment.NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      serviceConfig.KeepAlive = false;
+      serviceConfig.ProcessType = "Background";
+      serviceConfig.StartInterval = 360;
+    };
+  };
+
+  programs.nix-index.enable = true;
+  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent.enableSSHSupport = true;
+}
