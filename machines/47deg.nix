063e209ade490e818a1c83114406cb4903b9a6f4
Pepe García <pepe@pepegar.com>
2020-01-30 17:31:17 +0100

    moar

--- machines/47deg.nix
+++ machines/47deg.nix
@@ -1,30 +1,21 @@
 { config, pkgs, ... }:
 
-with pkgs;
-
-
-let
-  rescuetime-overlay = import ../overlays/rescuetime.nix;
-in rec {
+{
   imports = [
     ../applications/fzf
     ../applications/xmonad
+    ../applications/emacs
     ../applications/zsh
     ../applications/neovim
     ../applications/tmux
 
     ../cfg/git.nix
     ../cfg/ssh.nix
-    ../cfg/xresources.nix
     ../cfg/sbt
 
     ../services/dunst.nix
     ../services/gnome-keyring.nix
     ../services/blueman.nix
-  ];
-
-  nixpkgs.overlays = [
-    rescuetime-overlay
   ];
 
   # Let Home Manager install and manage itself.
@@ -39,7 +30,6 @@ in rec {
     pkgs.openvpn
     pkgs.dunst
     pkgs.sbt
-    pkgs.emacs
     pkgs.libreoffice
     /**pkgs.neovim */
     pkgs.stack
