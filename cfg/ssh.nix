caa78f1d3d1d10edeca15c043e747ab916321908
Pepe García <pepe@pepegar.com>
2020-02-13 18:36:55 +0100

    update ssh

--- cfg/ssh.nix
+++ cfg/ssh.nix
@@ -10,21 +10,33 @@
         port = 22822;
         user = "packdev";
       };
+      packHostTunnelConsul = name : packHost name // {
+        extraOptions = {
+          "LocalForward" = "localhost:8500 consul.service.consul:8500";
+        };
+      };
       packHostTunnelMongo = name : packHost name // {
         extraOptions = {
-          "LocalForward" = "27017 mongo.service.consul:27017";
+          "LocalForward" = "localhost:27017 mongo.service.consul:27017";
         };
       };
     in {
         # all these require VPN
+        ops = packHost "ops";
         xteam = packHost "xteamqa";
         fraggle = packHost "fraggleteamqa";
         lemmings = packHost "lemmingsteamqa";
         gremlin = packHost "gremlin";
+        ops-mongo = packHostTunnelMongo "ops";
         xteam-mongo = packHostTunnelMongo "xteamqa";
         fraggle-mongo = packHostTunnelMongo "fraggleteamqa";
         lemmings-mongo = packHostTunnelMongo "lemmingsteamqa";
         gremlin-mongo = packHostTunnelMongo "gremlin";
+        ops-consul = packHostTunnelConsul "ops";
+        xteam-consul = packHostTunnelConsul "xteamqa";
+        fraggle-consul = packHostTunnelConsul "fraggleteamqa";
+        lemmings-consul = packHostTunnelConsul "lemmingsteamqa";
+        gremlin-consul = packHostTunnelConsul "gremlin";
 
         keychain = {
           host = "*";
