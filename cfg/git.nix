f8c6d481bb72139f62c2ec042a3e66e0d126ff13
Pepe García <pepe@pepegar.com>
2020-03-17 11:43:28 +0100

    moar

--- cfg/git.nix
+++ cfg/git.nix
@@ -11,6 +11,12 @@
       github = {
         user = "pepegar";
       };
+
+      alias = {
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
+
+      ghq.root = "~/projects";
     };
   };
 }
