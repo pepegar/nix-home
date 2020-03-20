7ae77d9d3d3453d4e1765a071fd5e8cc98c7658c
Pepe García <pepe@pepegar.com>
2020-03-20 23:54:50 +0100

    create a jupyter-for-datascience overlay

--- machines/lisa.nix
+++ machines/lisa.nix
@@ -3,12 +3,12 @@
 with pkgs;
 
 let
-  rescuetime-overlay = import ../overlays/rescuetime.nix;
   python-packages = python-packages: with python-packages; [
     flask
     dash
   ];
   python-with-packages = pkgs.python3.withPackages python-packages;
+
 in rec {
   imports = [
     ../applications/fzf
@@ -54,5 +54,6 @@ in rec {
     pkgs.robo3t
     pkgs.spotify
     python-with-packages
+    pkgs.run-jupyter
   ];
 }
