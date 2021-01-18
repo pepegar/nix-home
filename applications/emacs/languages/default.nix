318d36fb84028dc17874d64625d8fa18ab5fd529
Pepe García <pepe@pepegar.com>
2021-01-18 12:39:51 +0100

    moar langs

--- applications/emacs/languages/default.nix
+++ applications/emacs/languages/default.nix
@@ -4,6 +4,8 @@
     ./nix
     ./scala
     ./python
+    ./jupyter
+    ./restclient
     ./org
   ];
 
@@ -29,27 +31,70 @@
 
     lsp-ui = {
       enable = true;
-      command = ["lsp-ui-mode"];
-      extraConfig = ''
-       :custom
-       (lsp-ui-doc-enable t)
-       (lsp-ui-doc-include-signture t)
-       (lsp-ui-doc-position 'top)
-       (lsp-ui-sideline-enable nil)
-     '';
+      command = [ "lsp-ui-mode" ];
+      bindLocal = {
+        lsp-mode-map = {
+          "C-c r d" = "lsp-ui-doc-glance";
+          "C-c f s" = "lsp-ui-find-workspace-symbol";
+        };
+      };
+      config = ''
+          (setq lsp-ui-sideline-enable t
+                lsp-ui-sideline-show-symbol nil
+                lsp-ui-sideline-show-hover nil
+                lsp-ui-sideline-show-code-actions nil
+                lsp-ui-sideline-update-mode 'point)
+          (setq lsp-ui-doc-enable nil
+                lsp-ui-doc-position 'at-point
+                lsp-ui-doc-max-width 120
+                lsp-ui-doc-max-height 15)
+        '';
+    };
+
+    lsp-ui-flycheck = {
+      enable = true;
+      after = [ "flycheck" "lsp-ui" ];
+    };
+
+    lsp-completion = {
+      enable = true;
+      after = [ "lsp-mode" ];
+      config = ''
+          (setq lsp-completion-enable-additional-text-edit nil)
+        '';
+    };
+
+    lsp-diagnostics = {
+      enable = true;
+      after = [ "lsp-mode" ];
     };
 
     lsp-mode = {
       enable = true;
-      hook = [
-        "(lsp-mode . lsp-enable-which-key-integration)"
-      ];
-      command = ["lsp"];
-      extraConfig = ''
-        :custom
-        (lsp-prefer-flymake t)
-        (lsp-enable-snippet nil)
-      '';
+      command = [ "lsp" ];
+      after = [ "company" "flycheck" ];
+      hook = [ "(lsp-mode . lsp-enable-which-key-integration)" ];
+      bindLocal = {
+        lsp-mode-map = {
+          "C-c r r" = "lsp-rename";
+          "C-c r f" = "lsp-format-buffer";
+          "C-c r g" = "lsp-format-region";
+          "C-c r a" = "lsp-execute-code-action";
+          "C-c f r" = "lsp-find-references";
+        };
+      };
+      init = ''
+          (setq lsp-keymap-prefix "C-c l")
+        '';
+      config = ''
+          (setq lsp-diagnostics-provider :flycheck
+                lsp-eldoc-render-all nil
+                lsp-headerline-breadcrumb-enable nil
+                lsp-modeline-code-actions-enable nil
+                lsp-modeline-diagnostics-enable nil
+                lsp-modeline-workspace-status-enable nil)
+          (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+        '';
     };
 
     lsp-treemacs = {
