f22a56fc05ca905e4cc0c7e025a7a0f3e6ed8ce2
Pepe García <pepe@pepegar.com>
2021-01-27 14:32:09 +0100

    pin neovim overlay

--- overlays/50-neovim.nix
+++ overlays/50-neovim.nix
@@ -1,3 +1,3 @@
 import (builtins.fetchTarball {
-  url = https://github.com/mjlbach/neovim-nightly-overlay/archive/master.tar.gz;
+  url = https://github.com/mjlbach/neovim-nightly-overlay/archive/c23fafc7adcf9bf740d3a17af994367633206d6b.tar.gz;
 })
