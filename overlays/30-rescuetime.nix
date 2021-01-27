423b01e821e8f8a53a6d152204092a956b9cfb9a
Pepe García <pepe@pepegar.com>
2021-01-27 15:54:09 +0100

    this is a rolling release, cannot be sha-checked

--- overlays/30-rescuetime.nix
+++ overlays/30-rescuetime.nix
@@ -5,7 +5,6 @@ self: super:
     version = "2.14.5.2";
     src = super.fetchurl {
       url = "https://www.rescuetime.com/installers/rescuetime_current_amd64.deb";
-      sha256 = "1a6pc8vi2ab721kzyhvg6bmw24dr85dgmx2m9j9vbf3jyr85fv10";
     };
   });
 }
