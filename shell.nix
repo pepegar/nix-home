72a5de02e1f554cc0f77bca46fb10fff825f7291
Pepe García <pepe@pepegar.com>
2021-01-27 17:26:20 +0100

    integrate pre-commit-hooks

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  nix-pre-commit-hooks = import (builtins.fetchTarball
+    "https://github.com/cachix/pre-commit-hooks.nix/tarball/master"); # can't really make this work with niv...
+  pre-commit-check = nix-pre-commit-hooks.run {
+    src = ./.;
+    hooks = { nixfmt.enable = true; };
+  };
+in pkgs.mkShell {
+  shellHook = ''
+    ${pre-commit-check.shellHook}
+  '';
+}
