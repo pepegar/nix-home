dbd0cfb08ea923712b92738388c8751d9baf39d0
Pepe García <pepe@pepegar.com>
2021-11-30 16:01:46 +0100

    create flake.nix

--- applications/direnv/default.nix
+++ applications/direnv/default.nix
@@ -7,4 +7,12 @@
     enableFishIntegration = true;
     nix-direnv.enable = true;
   };
+
+  home.file.".config/direnv/lib/use_flake.sh".text = ''
+    use_flake() {
+      watch_file flake.nix
+      watch_file flake.lock
+      eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
+    }
+      '';
 }
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "pepegar's nix home";
+
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system: {
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = { nixpkgs-fmt.enable = true; };
+        };
+      };
+      devShell = nixpkgs.legacyPackages.${system}.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+      };
+    });
+}
