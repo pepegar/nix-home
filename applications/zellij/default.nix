21e045ef677852025a599e40d12f958b093d07b0
Pepe García <pepe@pepegar.com>
2024-08-10 09:44:44 +0300

    add zellij aliases

--- applications/zellij/default.nix
+++ applications/zellij/default.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
   };
+
+  programs.zsh.initExtra = ''
+    eval "$(${pkgs.zellij}/bin/zellij setup --generate-completion zsh | tail -n13)"
+  '';
 
   home.file.".config/zellij/config.kdl".source = ./config.kdl;
 }
