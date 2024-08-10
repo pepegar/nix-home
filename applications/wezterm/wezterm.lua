d164d6f3171828bb2d4d6f3ada3e3e2707e682c4
Pepe García <pepe@pepegar.com>
2024-08-10 19:47:10 +0300

    maximize wezterm on startup

--- applications/wezterm/wezterm.lua
+++ applications/wezterm/wezterm.lua
@@ -1,5 +1,11 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
