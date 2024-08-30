3c6d758dd16cd195989663677af3f4afe4f95eaf
Pepe García <pepe@pepegar.com>
2024-08-30 14:50:21 +0100

    Let's not configure neovim from here for now

--- applications/neovim/default.nix
+++ applications/neovim/default.nix
@@ -1,81 +1,82 @@
-{ pkgs, ... }:
+{ ... }:
 
-let
-  custom-plugins = pkgs.callPackage ./custom-plugins.nix {
-    inherit (pkgs.vimUtils) buildVimPlugin;
-    inherit pkgs;
-  };
+#let
+#custom-plugins = pkgs.callPackage ./custom-plugins.nix {
+#inherit (pkgs.vimUtils) buildVimPlugin;
+#inherit pkgs;
+#};
 
-  myVimPlugins = [
-    pkgs.vimPlugins.emmet-vim
-    pkgs.vimPlugins.multiple-cursors
-    pkgs.vimPlugins.nerdcommenter
-    pkgs.vimPlugins.nerdtree
-    pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-    pkgs.vimPlugins.golden-ratio
-    pkgs.vimPlugins.rose-pine
-    pkgs.vimPlugins.tabular
-    pkgs.vimPlugins.telescope-nvim
-    pkgs.vimPlugins.telescope-frecency-nvim
-    pkgs.vimPlugins.telescope-fzy-native-nvim
-    pkgs.vimPlugins.trouble-nvim
-    pkgs.vimPlugins.gitsigns-nvim
-    pkgs.vimPlugins.lualine-nvim
-    pkgs.vimPlugins.lualine-lsp-progress
-    pkgs.vimPlugins.nvim-web-devicons
-    pkgs.vimPlugins.rainbow-delimiters-nvim
-    pkgs.vimPlugins.undotree
-    pkgs.vimPlugins.vim-css-color
-    pkgs.vimPlugins.vim-css-color
-    pkgs.vimPlugins.vim-easy-align
-    pkgs.vimPlugins.vim-easymotion
-    pkgs.vimPlugins.vim-fish
-    pkgs.vimPlugins.vim-nix
-    pkgs.vimPlugins.vim-repeat
-    pkgs.vimPlugins.vim-rhubarb
-    pkgs.vimPlugins.vim-sensible
-    pkgs.vimPlugins.vim-surround
-    pkgs.vimPlugins.lsp-zero-nvim
-    pkgs.vimPlugins.nvim-lspconfig
-    pkgs.vimPlugins.nvim-cmp
-    pkgs.vimPlugins.cmp-buffer
-    pkgs.vimPlugins.cmp-path
-    pkgs.vimPlugins.cmp_luasnip
-    pkgs.vimPlugins.cmp-nvim-lsp
-    pkgs.vimPlugins.cmp-nvim-lua
-    pkgs.vimPlugins.luasnip
-    pkgs.vimPlugins.friendly-snippets
-    pkgs.vimPlugins.nvim-spectre
-    pkgs.vimPlugins.neoformat
-    pkgs.vimPlugins.oil-nvim
-    pkgs.vimPlugins.autoclose-nvim
-    pkgs.vimPlugins.nvim-ts-autotag
-    pkgs.vimPlugins.neogit
+#myVimPlugins = [
+#pkgs.vimPlugins.emmet-vim
+#pkgs.vimPlugins.multiple-cursors
+#pkgs.vimPlugins.nerdcommenter
+#pkgs.vimPlugins.nerdtree
+#pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+#pkgs.vimPlugins.golden-ratio
+#pkgs.vimPlugins.rose-pine
+#pkgs.vimPlugins.tabular
+#pkgs.vimPlugins.telescope-nvim
+#pkgs.vimPlugins.telescope-frecency-nvim
+#pkgs.vimPlugins.telescope-fzy-native-nvim
+#pkgs.vimPlugins.trouble-nvim
+#pkgs.vimPlugins.gitsigns-nvim
+#pkgs.vimPlugins.lualine-nvim
+#pkgs.vimPlugins.lualine-lsp-progress
+#pkgs.vimPlugins.nvim-web-devicons
+#pkgs.vimPlugins.rainbow-delimiters-nvim
+#pkgs.vimPlugins.undotree
+#pkgs.vimPlugins.vim-css-color
+#pkgs.vimPlugins.vim-css-color
+#pkgs.vimPlugins.vim-easy-align
+#pkgs.vimPlugins.vim-easymotion
+#pkgs.vimPlugins.vim-fish
+#pkgs.vimPlugins.vim-nix
+#pkgs.vimPlugins.vim-repeat
+#pkgs.vimPlugins.vim-rhubarb
+#pkgs.vimPlugins.vim-sensible
+#pkgs.vimPlugins.vim-surround
+#pkgs.vimPlugins.lsp-zero-nvim
+#pkgs.vimPlugins.nvim-lspconfig
+#pkgs.vimPlugins.nvim-cmp
+#pkgs.vimPlugins.cmp-buffer
+#pkgs.vimPlugins.cmp-path
+#pkgs.vimPlugins.cmp_luasnip
+#pkgs.vimPlugins.cmp-nvim-lsp
+#pkgs.vimPlugins.cmp-nvim-lua
+#pkgs.vimPlugins.luasnip
+#pkgs.vimPlugins.friendly-snippets
+#pkgs.vimPlugins.nvim-spectre
+#pkgs.vimPlugins.neoformat
+#pkgs.vimPlugins.oil-nvim
+#pkgs.vimPlugins.autoclose-nvim
+#pkgs.vimPlugins.nvim-ts-autotag
+#pkgs.vimPlugins.neogit
 
-    custom-plugins.telescope-ghq
-    custom-plugins.kdl
-  ];
+#custom-plugins.telescope-ghq
+#custom-plugins.kdl
+#];
 
-  wrapLuaConfig = str: ''
-    lua << EOF
-    ${str}
-    EOF
-  '';
+#wrapLuaConfig = str: ''
+#lua << EOF
+#${str}
+#EOF
+#'';
 
-  treesitterConfig = wrapLuaConfig (builtins.readFile ./treesitter.lua);
-  lualineConfig = wrapLuaConfig (builtins.readFile ./lualine.lua);
-  lspConfig = wrapLuaConfig (builtins.readFile ./lsp.lua);
-  telescopeConfig = wrapLuaConfig (builtins.readFile ./telescope.lua);
-  basicsConfig = wrapLuaConfig (builtins.readFile ./basics.lua);
-  gitsignsConfig = wrapLuaConfig (builtins.readFile ./gitsigns.lua);
-  troubleConfig = wrapLuaConfig (builtins.readFile ./trouble.lua);
-  undotreeConfig = wrapLuaConfig (builtins.readFile ./undotree.lua);
-  neoformatConfig = wrapLuaConfig (builtins.readFile ./neoformat.lua);
-  oilConfig = wrapLuaConfig (builtins.readFile ./oil.lua);
-  autotagConfig = wrapLuaConfig (builtins.readFile ./autotag.lua);
-  autocloseConfig = wrapLuaConfig (builtins.readFile ./autoclose.lua);
-  neogitConfig = wrapLuaConfig (builtins.readFile ./neogit.lua);
-in {
+#treesitterConfig = wrapLuaConfig (builtins.readFile ./treesitter.lua);
+#lualineConfig = wrapLuaConfig (builtins.readFile ./lualine.lua);
+#lspConfig = wrapLuaConfig (builtins.readFile ./lsp.lua);
+#telescopeConfig = wrapLuaConfig (builtins.readFile ./telescope.lua);
+#basicsConfig = wrapLuaConfig (builtins.readFile ./basics.lua);
+#gitsignsConfig = wrapLuaConfig (builtins.readFile ./gitsigns.lua);
+#troubleConfig = wrapLuaConfig (builtins.readFile ./trouble.lua);
+#undotreeConfig = wrapLuaConfig (builtins.readFile ./undotree.lua);
+#neoformatConfig = wrapLuaConfig (builtins.readFile ./neoformat.lua);
+#oilConfig = wrapLuaConfig (builtins.readFile ./oil.lua);
+#autotagConfig = wrapLuaConfig (builtins.readFile ./autotag.lua);
+#autocloseConfig = wrapLuaConfig (builtins.readFile ./autoclose.lua);
+#neogitConfig = wrapLuaConfig (builtins.readFile ./neogit.lua);
+# in {
+{
   programs.neovim = {
     enable = true;
     viAlias = true;
@@ -83,10 +84,10 @@ in {
     vimdiffAlias = true;
     withNodeJs = true;
     withPython3 = true;
-    plugins = myVimPlugins;
-    extraConfig = basicsConfig + treesitterConfig + telescopeConfig
-      + lualineConfig + lspConfig + gitsignsConfig + troubleConfig
-      + undotreeConfig + neoformatConfig + oilConfig + autotagConfig
-      + autocloseConfig + neogitConfig;
+    #plugins = myVimPlugins;
+    #extraConfig = basicsConfig + treesitterConfig + telescopeConfig
+    #+ lualineConfig + lspConfig + gitsignsConfig + troubleConfig
+    #+ undotreeConfig + neoformatConfig + oilConfig + autotagConfig
+    #+ autocloseConfig + neogitConfig;
   };
 }
--- applications/neovim/lsp.lua
+++ applications/neovim/lsp.lua
@@ -47,10 +47,88 @@ lsp.lua_ls.setup({
 })
 lsp.pyright.setup({ capabilities = capabilities })
 lsp.tsserver.setup({ capabilities = capabilities })
-lsp.ruff.setup({
-	init_options = {
-		settings = {
-			args = {},
+lsp.ruff.setup({ init_options = { settings = { args = {} } } })
+
+local configs = require("lspconfig.configs")
+
+if not configs.ideals then
+	configs.ideals = {
+		default_config = {
+			--cmd = { "idea", "lsp-server" },
+			cmd = { "nc", "localhost", "8989" },
+			filetypes = { "kotlin", "java" },
+			root_dir = function(fname)
+				return lsp.util.find_git_ancestor(fname)
+			end,
+			settings = {},
 		},
+	}
+end
+
+lsp.ideals.setup({})
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+		end,
 	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- Select the [n]ext item
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		-- Select the [p]revious item
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+
+		-- Scroll the documentation window [b]ack / [f]orward
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+		-- Accept ([y]es) the completion.
+		--  This will auto-import if your LSP supports it.
+		--  This will expand snippets if the LSP sent a snippet.
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+		-- If you prefer more traditional completion keymaps,
+		-- you can uncomment the following lines
+		--['<CR>'] = cmp.mapping.confirm { select = true },
+		--['<Tab>'] = cmp.mapping.select_next_item(),
+		--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+		-- Manually trigger a completion from nvim-cmp.
+		--  Generally you don't need this, because nvim-cmp will display
+		--  completions whenever it has completion options available.
+		["<C-Space>"] = cmp.mapping.complete({}),
+
+		-- Think of <c-l> as moving to the right of your snippet expansion.
+		--  So if you have a snippet that's like:
+		--  function $name($args)
+		--    $body
+		--  end
+		--
+		-- <c-l> will move you to the right of each of the expansion locations.
+		-- <c-h> is similar, except moving you backwards.
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+	}, {
+		{ name = "buffer" },
+	}),
 })
