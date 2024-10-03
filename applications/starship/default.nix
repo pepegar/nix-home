5fe75b6c3f3b6c99f829cf9680b20eed03459a6b
Pepe Garcia <pepe@pepegar.com>
2024-10-03 17:56:47 +0200

    starship stuff

--- applications/starship/default.nix
+++ applications/starship/default.nix
@@ -1,11 +1,17 @@
-{ ... }:
-
-{
+{...}: {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    enableBashIntegration = true;
 
     settings = {
+      format = "$directory$git_branch$\{custom.git_branch_description\}$nix_shell$aws$fill$line_break$character";
+      custom.git_branch_description = {
+        command = "git config branch.$(git rev-parse --abbrev-ref HEAD 2>/dev/null).description";
+        when = "git rev-parse --is-inside-work-tree 2>/dev/null";
+        format = "[ - $output]($style) ";
+        style = "bold yellow";
+      };
       command_timeout = 1000;
       directory.read_only = " ";
       battery = {
@@ -14,12 +20,11 @@
         discharging_symbol = "⇣";
       };
       git_branch = {
-        format = "[$branch]($style)";
+        format = "[\\[$branch\\]]($style)";
         style = "bright-black";
       };
       git_status = {
-        format =
-          "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](bright-black)( $ahead_behind$stashed)]($style) ";
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](bright-black)( $ahead_behind$stashed)]($style) ";
         style = "cyan";
         conflicted = "​";
         untracked = "​";
@@ -37,7 +42,6 @@
         format = "[$duration]($style) ";
         style = "yellow";
       };
-      #kubernetes = { disabled = false; };
     };
   };
 }
