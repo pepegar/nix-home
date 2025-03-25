1e4553f57dc800f5cff1323fe32dad2be8e21248
Pepe Garcia <pepe@pepegar.com>
2025-03-25 12:04:21 +0000

    create IJ config files for all IJ editors

--- applications/intellij-idea/default.nix
+++ applications/intellij-idea/default.nix
@@ -1,6 +1,25 @@
-{ ... }:
-
 {
-  home.file."Library/Application Support/JetBrains/IntelliJIdea2023.3/idea.properties".source =
-    ./idea.properties;
+  config,
+  lib,
+  ...
+}: let
+  safeReadDir = path:
+    if builtins.pathExists path
+    then builtins.readDir path
+    else {};
+
+  jetbrainsDir = "${config.home.homeDirectory}/Library/Application Support/JetBrains";
+
+  jetbrainsProducts = let
+    dirContents = safeReadDir jetbrainsDir;
+    # Filter to only include directories
+    dirNames = builtins.attrNames (lib.filterAttrs (_name: type: type == "directory") dirContents);
+  in
+    dirNames;
+in {
+  home.file = builtins.listToAttrs (map (version: {
+      name = "Library/Application Support/JetBrains/${version}/idea.properties";
+      value = {source = ./idea.properties;};
+    })
+    jetbrainsProducts);
 }
