4e3a3d22fe3d15a437be55c2c257b74698aa476c
Pepe Garcia <pepe@pepegar.com>
2025-08-31 23:38:58 +0100

    lotsa karabinix changes

--- applications/karabinix/default.nix
+++ applications/karabinix/default.nix
@@ -1,99 +1,77 @@
-{inputs, ...}: {
+{inputs, ...}:
+with inputs.karabinix.lib; {
   services.karabinix = {
     enable = true;
-    configuration = with inputs.karabinix.lib; {
+    configuration = {
       profiles = [
         (mkProfile {
           name = "created with karabinix";
           selected = true;
 
           complex_modifications = mkComplexModification {
+            parameters = {
+              "basic.simultaneous_threshold_milliseconds" = 50;
+              "basic.to_if_alone_timeout_milliseconds" = 1000;
+              "basic.to_if_held_down_threshold_milliseconds" = 500;
+              "basic.to_delayed_action_delay_milliseconds" = 500;
+            };
             rules = [
               (hyperKey {
                 key = keyCodes.caps_lock;
                 alone_key = keyCodes.escape;
                 mappings = {
+                  # apps
                   t = mkToEvent {shell_command = "open -a 'Ghostty'";};
                   b = mkToEvent {shell_command = "open -a 'Arc'";};
+                  i = mkToEvent {shell_command = "open -a 'IntelliJ IDEA'";};
+
+                  # window management
+                  f = raycastWindow "maximize";
+                  h = raycastWindow "left-half";
+                  l = raycastWindow "right-half";
+                  k = raycastWindow "top-half";
+                  j = raycastWindow "bottom-half";
                 };
               })
 
-              # IntelliJ IDEA layer
-              (layerKey {
-                key = keyCodes.i;
-                variable_name = "intellij_layer";
-                conditions = [
-                  (appCondition ["com.jetbrains.intellij" "com.jetbrains.intellij.ce"] "frontmost_application_if")
-                ];
-                mappings = {
-                  # Navigation: Previous shortcuts (i+p m, i+p e)
-                  p = {
-                    m = mkToEvent {
-                      key_code = keyCodes.up_arrow;
-                      modifiers = ["left_option" "left_command"];
-                    };
-                    e = mkToEvent {
-                      key_code = keyCodes.f2;
-                      modifiers = ["left_shift"];
-                    };
-                  };
-
-                  # Navigation: Next shortcuts (i+n m, i+n e)
-                  n = {
+              (appLayerKey {
+                key = keyCodes.spacebar;
+                variable_name = "per_app_layer";
+                alone_key = keyCodes.spacebar;
+                app_mappings = {
+                  "com.jetbrains.intellij" = {
                     m = mkToEvent {
                       key_code = keyCodes.down_arrow;
-                      modifiers = ["left_option" "left_command"];
+                      modifiers = ["left_control" "left_shift"];
                     };
-                    e = mkToEvent {
+
+                    "shift+m" = mkToEvent {
+                      key_code = keyCodes.up_arrow;
+                      modifiers = ["left_control" "left_shift"];
+                    };
+
+                    n = mkToEvent {
                       key_code = keyCodes.f2;
+                      modifiers = ["fn"];
                     };
-                  };
 
-                  # Debugging shortcuts with mnemonics
-                  b = mkToEvent {
-                    # Toggle Breakpoint
-                    key_code = keyCodes.f8;
-                    modifiers = ["left_command"];
-                  };
-                  r = mkToEvent {
-                    # Debug Run
-                    key_code = keyCodes.f9;
-                    modifiers = ["left_shift" "left_command"];
-                  };
-                  s = mkToEvent {
-                    # Step Over
-                    key_code = keyCodes.f8;
-                  };
-                  i = mkToEvent {
-                    # Step Into
-                    key_code = keyCodes.f7;
-                  };
-                  o = mkToEvent {
-                    # Step Out
-                    key_code = keyCodes.f8;
-                    modifiers = ["left_shift"];
-                  };
-                  c = mkToEvent {
-                    # Continue/Resume
-                    key_code = keyCodes.f9;
-                    modifiers = ["left_command"];
-                  };
+                    "shift+n" = mkToEvent {
+                      key_code = keyCodes.f2;
+                      modifiers = ["fn" "left_shift"];
+                    };
 
-                  # Additional navigation shortcuts
-                  g = mkToEvent {
-                    # Go to Line
-                    key_code = keyCodes.g;
-                    modifiers = ["left_command"];
-                  };
-                  f = mkToEvent {
-                    # Find File
-                    key_code = keyCodes.o;
-                    modifiers = ["left_command" "left_shift"];
-                  };
-                  t = mkToEvent {
-                    # Go to Test
-                    key_code = keyCodes.n;
-                    modifiers = ["left_command" "left_shift"];
+                    # Debugging shortcuts with mnemonics
+                    b = mkToEvent {
+                      # Toggle Breakpoint
+                      key_code = keyCodes.f8;
+                      modifiers = ["fn" "left_command"];
+                    };
+
+                    e = mkToEvent {
+                      # file structure
+                      key_code = keyCodes.f12;
+                      modifiers = ["fn" "left_command"];
+                    };
                   };
                 };
               })
