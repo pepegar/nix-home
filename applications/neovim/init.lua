a0758710e1ef2b37b45ed39434aa9ba6cc3094b4
Pepe Garcia <pepe@pepegar.com>
2025-09-09 11:59:30 +0100

    cs'"

--- applications/neovim/init.lua
+++ applications/neovim/init.lua
@@ -417,6 +417,14 @@ require("lazy").setup({
 	},
 
 	"preservim/nerdcommenter",
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 
 	{
 		"NeogitOrg/neogit",
