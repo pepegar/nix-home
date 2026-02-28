2626ed1d86f13933be06b9e226da1240a819516e
Pepe Garcia <pepe@pepegar.com>
2026-02-28 05:30:45 +0000

    moar

--- applications/opencode/default.nix
+++ applications/opencode/default.nix
@@ -11,12 +11,8 @@
           apiKey = secrets.LITELLM_AUTH_TOKEN;
         };
         models = {
-          "anthropic/claude-opus-4-5" = {
-            name = "Claude Opus 4.5";
-            attachment = true;
-          };
-          "anthropic/claude-haiku-4-5-20251001" = {
-            name = "Claude Haiku 4.5";
+          "anthropic/claude-opus-4-6" = {
+            name = "Claude Opus 4.6";
             attachment = true;
           };
         };
